fixes in scaling, keras categorical, cleanup

play.py
@@ -179,34 +179,12 @@ class Actor(object):
         manual_override = self.real_controller.RightThumb == 1
 
         if not manual_override:
-        # if True:
             ## Look
-            # print("debug")
-            # print(img)
             vec = img
             vec = np.expand_dims(vec, axis=0) # expand dimensions for predict, it wants (1,66,200,3) not (66, 200, 3)
             ## Think
             # joystick = self.model.predict(vec, batch_size=1)[0]
             joystick = categorical_model_predict(self.model, vec)
-            
-            # for i,num in enumerate(joystick):
-            #     if joystick[i] >= self.thresh: # predicting 1, positive
-            #         joystick[i] = 1
-            #     elif -self.thresh < joystick[i] < self.thresh: # predicting 0, nothing
-            #         joystick[i] = 0
-            #     elif joystick[i] <= -self.thresh: # predicting -1, negative
-            #         joystick[i] = -1
-            #     else:
-            #         print("Error")
-
-            # joystick = joystick * 3
-            # for i,num in enumerate(joystick):
-            #     if joystick[i] > 1:
-            #         joystick[i] = 1
-            #     elif joystick[i] < -1:
-            #         joystick[i] = -1
-            #     else:
-            #         pass
             
             print(joystick)
 
@@ -235,7 +213,7 @@ if __name__ == '__main__':
     kernel32 = ctypes.windll.kernel32
     kernel32.SetThreadPriority(kernel32.GetCurrentThread(), 31) 
 
-    #disable gpu
+    #disable gpu memory growth
     gpus = tf.config.list_physical_devices("GPU")
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu,True)

train.py
@@ -15,7 +15,7 @@ from tensorflow.keras.layers import TimeDistributed
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras import optimizers, Model, Input
 from tensorflow.keras import backend as K
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 from utils import Sample, load_data_from_samples
 
 # Global variable
@@ -284,15 +284,17 @@ def train_categorical_model(x_train, y_train, _model=categorical_model, batch_si
     # Binning and one-hot encoding
     print("Binning and one-hot encoding")
     num_bins = 15
-    bin_edges = np.linspace(-1, 1, num_bins + 1)
-    y_binned = np.digitize(y_train, bin_edges)
-    y_train = np.eye(num_bins)[y_binned - 1] 
-    print("y_train shape: ", y_train.shape)
+    # bin_edges = np.linspace(-1, 1, num_bins + 1)
+    # y_binned = np.digitize(y_train, bin_edges)
+    # y_train = np.eye(num_bins)[y_binned - 1] 
+    # print("y_train shape: ", y_train.shape)
+    y_train = tf.keras.utils.to_categorical(y_train, num_bins)
 
     model = _model()
 
-    checkpoint = ModelCheckpoint("model_weights_c1.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
-    callbacks_list = [checkpoint]
+    checkpoint = ModelCheckpoint("model_weights_c2a.h5", monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    tensorboard = TensorBoard(log_dir="logs_C/", histogram_freq=0, write_graph=True, write_images=True)
+    callbacks_list = [checkpoint, tensorboard]
 
     # default lr=0.001, make it smaller
     model.compile(loss="categorical_crossentropy", optimizer=optimizers.Adam(lr=0.00002))
@@ -334,7 +336,9 @@ if __name__ == '__main__':
     print("loading training data")
     # x_train = np.load("data/x_sbal.npy")
     # y_train = np.load("data/y_sbal.npy")
-    x_train, y_train = load_data_from_samples(['samples/forza4003', 'samples/forza4004', 'samples/forza4005'], debug=True, augment=True)
+    # samples = ['samples/forza4003', 'samples/forza4004', 'samples/forza4005']
+    samples = ['samples/forza4003']
+    x_train, y_train = load_data_from_samples(samples, debug=False, augment=True)
 
     print(x_train.shape[0], 'train samples')
 

utils.py
@@ -298,7 +298,7 @@ def ask_for_samples():
     # ask for samples
     samples = eval(input("Enter sample paths to load: "))
     # load data
-    return load_data_from_samples(samples)
+    return load_data_from_samples(samples, augment=False)
 
 def plot_data(y_pth, predictions=False, model_pth=None, x_pth=None, categorical=False):
     categorical = True if categorical == "y" else False
@@ -365,9 +365,9 @@ def plot_data(y_pth, predictions=False, model_pth=None, x_pth=None, categorical=
         model.load_weights(model_pth)
         # predict
         y_preds = []
-        t0 = time.perf_counter()
+        t0 = time.perf_counter(); y_pred = 0
         for i, x in enumerate(X):
-            print(i, "/", len(X)-1, end="\r")
+            print(i, "/", len(X)-1, y_pred, end="\r")
             y_pred = categorical_model_predict(model, np.expand_dims(x, axis=0))
             y_preds.append(y_pred)
         t1 = time.perf_counter()
@@ -650,7 +650,7 @@ def prepare(samples, augment=True):
     
     return
 
-def load_data_from_samples(paths, augment=True, debug=False):
+def load_data_from_samples(paths, augment=True, debug=False, generator=False):
     INPUT_SHAPE = (Sample.IMG_H, Sample.IMG_W, Sample.IMG_D)
 
     # for each path, load y data from data.csv
@@ -679,12 +679,12 @@ def load_data_from_samples(paths, augment=True, debug=False):
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
                 # resize image
                 img = cv2.resize(img, (INPUT_SHAPE[1], INPUT_SHAPE[0]))
+                img = img.astype(np.float32)
+                img /= 255.0
                 if debug and i % 500 == 0:
                     plt.imshow(img)
                     plt.title(tokens[1]+" "+str(i))
                     plt.show()
-                img = img.astype(np.float32)
-                img = img / 127.5 - 1.0
                 x[i] = img
                 y[i] = float(tokens[1])
                 if augment:
@@ -701,7 +701,12 @@ def load_data_from_samples(paths, augment=True, debug=False):
                 print(f"sample {i} of {num_samples}", end="\r")
                 i += 1
     
+                # if generator:
+                #     yield x[i], y[i]
+    # if not generator:
+    #     return x, y
     return x, y
+
 
 def build_sorted_dataset(dataset_path: str, samples_paths: list):
     # build sorted dataset at samples/path